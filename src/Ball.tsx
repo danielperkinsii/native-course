fee43e6b2282b30f82315bb61f3ebbefd7e75223
daniel perkins <daniel.perkins99@gmail.com>
2025-06-28 14:22:03 -0500

    create ball component

---
+++ src/Ball.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Ball = () => {
+    return (
+        <View style={styles.ball} /> 
+    )
+}
+
+const styles = StyleSheet.create({
+    ball: {
+        height: 60,
+        width: 60,
+        borderRadius: 30,
+        borderColor: 'black',
+        backgroundColor: 'black',
+
+    }
+});
+
+export default Ball
