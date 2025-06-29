5e22ff8f8ff47b1f141460bfeffee9eb1d686639
daniel perkins <daniel.perkins99@gmail.com>
2025-06-29 16:48:38 -0500

    adds cards and deck

--- App.tsx
+++ App.tsx
@@ -1,11 +1,55 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Ball from "./src/Ball";
+import Deck from "./src/Deck";
+import { Card, Button } from "@rneui/themed";
+
+const DATA = [
+  {
+    id: 1,
+    text: "Card #1",
+    uri: "https://images.pexels.com/photos/1266810/pexels-photo-1266810.jpeg?auto=compress&cs=tinysrgb&w=800",
+  },
+  {
+    id: 2,
+    text: "Card #2",
+    uri: "https://images.pexels.com/photos/1287145/pexels-photo-1287145.jpeg?auto=compress&cs=tinysrgb&w=800",
+  },
+  {
+    id: 3,
+    text: "Card #3",
+    uri: "https://images.pexels.com/photos/2387873/pexels-photo-2387873.jpeg?auto=compress&cs=tinysrgb&w=800",
+  },
+  {
+    id: 4,
+    text: "Card #4",
+    uri: "https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&w=800",
+  },
+  {
+    id: 5,
+    text: "Card #5",
+    uri: "https://images.pexels.com/photos/2559941/pexels-photo-2559941.jpeg?auto=compress&cs=tinysrgb&w=800",
+  },
+  {
+    id: 6,
+    text: "Card #6",
+    uri: "https://images.pexels.com/photos/2387418/pexels-photo-2387418.jpeg?auto=compress&cs=tinysrgb&w=800",
+  },
+  {
+    id: 7,
+    text: "Card #7",
+    uri: "https://images.pexels.com/photos/3244513/pexels-photo-3244513.jpeg?auto=compress&cs=tinysrgb&w=800",
+  },
+  {
+    id: 8,
+    text: "Card #8",
+    uri: "https://images.pexels.com/photos/3293148/pexels-photo-3293148.jpeg?auto=compress&cs=tinysrgb&w=800",
+  },
+];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Ball />
+      <Deck data={DATA} />
     </View>
   );
 }
@@ -14,5 +58,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  cardText: {
+    backgroundColor: "blue",
+    color: "red",
   },
 });
---
+++ src/Deck.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { View, Animated, Text, StyleSheet, PanResponder } from "react-native";
+import { Card, Button } from "@rneui/themed";
+
+const Deck = ({ data, renderCard }: any) => {
+  return (
+    <View>
+      {data.map((item: any, i: number) => (
+        <Card>
+          <Card.Title>{item.text}</Card.Title>
+          <Card.Image
+            style={styles.avatar}
+            resizeMode="cover"
+            source={{ uri: item.uri }}
+          />
+          <Button
+            icon={{ name: "code" }}
+            style={styles.button}
+            title={"view now!"}
+          ></Button>
+        </Card>
+      ))}
+    </View>
+  );
+};
+
+export default Deck;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 10,
+    alignItems: "center",
+  },
+  card: {
+    width: "90%", // Adjust card width as needed
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "#03A9F4",
+  },
+  userContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 15,
+  },
+  userInfo: {
+    flex: 1,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  jobTitle: {
+    fontSize: 14,
+    color: "gray",
+  },
+});
